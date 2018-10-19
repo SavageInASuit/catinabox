3297eb9f98f0ac4d0d209038b66524f3b12af544
SavageInASuit <t89savage@gmail.com>
2018-10-19 17:09:39 +0100

    Improved test coverage

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,18 +2,25 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(5) == 25
+    assert catmath.cat_years_to_hooman_years(10) == 50
+    assert catmath.cat_years_to_hooman_years(28) == 140
+    assert catmath.cat_years_to_hooman_years(31) == 155
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0.5) == 2.5
+    assert catmath.cat_years_to_hooman_years(0.2) == 1
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
+    assert catmath.cat_years_to_hooman_years(12) != 0
 
 
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
+    assert catmath.is_cat_leap_year(2040) is True
+    assert catmath.is_cat_leap_year(2041) is False
